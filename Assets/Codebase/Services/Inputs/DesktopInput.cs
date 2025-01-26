93f99d56452841dad71050b5e2fa260bc437a1a3
KiselevAlexey <alekskisliy@gmail.com>
2025-01-26 20:27:20 +0200

    Add my TestScene and Work InputGame

--- Assets/Codebase/Services/Inputs/DesktopInput.cs
+++ Assets/Codebase/Services/Inputs/DesktopInput.cs
@@ -4,8 +4,7 @@ namespace Codebase.Services.Inputs
 {
     public class DesktopInput : IInputService
     {
-        public bool Left => Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow);
-        public bool Right => Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow);
+        public float Horizontal => Input.GetAxisRaw("Horizontal");
         public bool Jump => Input.GetKeyDown(KeyCode.Space);
         public bool Slide => Input.GetKeyDown(KeyCode.S);
         public bool Fire => Input.GetKeyDown(KeyCode.Mouse0);
--- Assets/Codebase/Services/Inputs/IInputService.cs
+++ Assets/Codebase/Services/Inputs/IInputService.cs
@@ -2,8 +2,7 @@ namespace Codebase.Services.Inputs
 {
     public interface IInputService
     {
-        public bool Left { get; }
-        public bool Right { get; }
+        public float Horizontal { get; }
         public bool Jump { get; }
         public bool Slide { get; }
         public bool Fire { get; }
