848d96e4de06f655455abdbdd518bcf26ebd09ea
KiselevAlexey <alekskisliy@gmail.com>
2025-02-01 22:07:36 +0200

    Added MainMenu and SkyBox

---
+++ Assets/Codebase/Components/Helpers/SkyboxRotation.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace Codebase.Components.Helpers
+{
+    public class SkyboxRotation : MonoBehaviour
+    {
+        public Material skybox;
+        public float angleSpeed = 1f;
+        public Gradient gradient;
+
+        private float _angle;
+
+        private static readonly int Tint = Shader.PropertyToID("_Tint");
+        private static readonly int Rotation = Shader.PropertyToID("_Rotation");
+
+        private void Update()
+        {
+            _angle += angleSpeed * Time.deltaTime;
+
+            if (_angle > 360f)
+            {
+                _angle = 0f;
+            }
+
+            skybox.SetColor(Tint, gradient.Evaluate(_angle / 360f));
+            skybox.SetFloat(Rotation, _angle);
+        }
+    }
+}
